2655d0da77268f934ca59ac877d88b6e692fc0fd
matias <matiasfr@usc.edu>
2015-10-31 21:22:21 -0700

    tweaked new audio manager. nullified inappropriate sound stubs. stage-specific sounds now set in their prefab

--- Assets/__Scripts/StageManager.cs
+++ Assets/__Scripts/StageManager.cs
@@ -6,12 +6,16 @@ public class StageManager : MonoBehaviour {
     public static StageManager Instance;
     public float DeathHeight = -15f;
 	// Use this for initialization
+	public AudioClip AmbientSFX;
+	public AudioClip MusicSFX;
 
     void Awake() {
         Instance = this;
     }
+
 	void Start () {
-	
+		AudioManager.instance.PlayAmbient(AmbientSFX);
+		AudioManager.instance.PlayMusic(MusicSFX);
 	}
 	
 	// Update is called once per frame
